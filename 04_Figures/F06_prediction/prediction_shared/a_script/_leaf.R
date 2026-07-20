ecbfd26aafe4ee3ce1a28749e83db1836c2fef19
Dustyn-T-Lewis <dtlts14@gmail.com>
2026-07-20 10:51:52 -0500

    move the prediction harness up to functions/shared_prediction.R

--- 04_Figures/F06_prediction/prediction_shared/a_script/_leaf.R
+++ 04_Figures/F06_prediction/prediction_shared/a_script/_leaf.R
@@ -7,7 +7,7 @@ pacman::p_load(here, dplyr, readr, ggplot2)
 
 source(here("functions", "shared_style.R"))
 source(here("04_Figures", "F06_prediction", "prediction_shared", "a_script", "_features.R"))
-source(here("04_Figures", "F06_prediction", "prediction_shared", "a_script", "_harness.R"))
+source(here("functions", "shared_prediction.R"))
 source(here("04_Figures", "F06_prediction", "prediction_shared", "a_script", "_panels.R"))
 
 PRED_SPACES <- c("singscore", "proteins", "eigengenes")
---
+++ tests/testthat/test-shared-prediction.R
@@ -0,0 +1,21 @@
+test_that("shared_prediction harness is deterministic and leakage-free", {
+  source(here::here("functions", "shared_prediction.R"))
+
+  set.seed(1)
+  x <- matrix(rnorm(20 * 6), nrow = 20, dimnames = list(paste0("s", 1:20), NULL))
+  y <- as.numeric(x[, 1] > 0)
+
+  preds1 <- nested_loso(x, y, "glmnet", "binomial")
+  preds2 <- nested_loso(x, y, "glmnet", "binomial")
+  expect_identical(preds1, preds2)
+  expect_length(preds1, 20)
+
+  # perm_matrix must be reproducible from its seed alone
+  expect_identical(perm_matrix(20, nperm = 5), perm_matrix(20, nperm = 5))
+
+  # align_xy drops zero-variance columns and NA outcomes
+  xz <- cbind(x, const = 1)
+  al <- align_xy(xz, setNames(c(y[1:19], NA), rownames(xz)))
+  expect_false("const" %in% colnames(al$x))
+  expect_equal(nrow(al$x), 19)
+})
